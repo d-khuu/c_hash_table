eacde3bbe2d4088d08b7749c6f5abf8826fa4762
s3601030 <s3601030@student.rmit.edu.au>
2023-03-23 21:46:53 +1100

    Added main/debugging code for testing

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     
     int SIZE_G = atoi(argv[1]);
     int exitCode;
-    
+
     exitCode = isPrime(SIZE_G);
     if(exitCode == 1)
     {
@@ -39,23 +39,34 @@ int main(int argc, char *argv[])
         SIZE_G = generatePrime(SIZE_G);
         printf("We'll generate one for you. Here, it will be %d.\n", SIZE_G);
     }
-    // char* str = "dude";
-
-    // int result = hash(str);
-
-    // printf("Result: %d for string %s\n", result, str);
     
     ht_hash_table* hashTablePtr = malloc(sizeof(*hashTablePtr));
 
-    exitCode = initialiseHashTable(hashTablePtr, SIZE_G);
-
-    if(exitCode != 0)
+    if(initialiseHashTable(hashTablePtr, SIZE_G) != 0)
     {
         printf("Error: Failed to initialise the hash table.\n");
     }
-    // printf("Exit Code: %d\n", exitCode);
-
     printf("Hash Table Size: %d, Count: %d.\n", hashTablePtr->size, hashTablePtr->count);
 
+    char * keyTest = "donut";
+    void * valTest = "valueTest";
+    printf("%s\n",keyTest);
+
+    char * toon = "toonj";
+    void * val = "kingdom";
+    printf("hash of new is %d\n", hash(toon, hashTablePtr->size));
+    
+    insert(hashTablePtr, keyTest, valTest);
+    insert(hashTablePtr, toon, val);
+    // keyTest = (char *)malloc(20);
+    // strcpy(keyTest, "fake");
+    printf("%s\n",keyTest);
+    printf("%d\n",hash(keyTest, hashTablePtr->size));
+    printf("key => %s = %d, item => %s\n", keyTest, hash(keyTest, hashTablePtr->size), hashTablePtr->items[hash(keyTest, hashTablePtr->size)]->value);
+    if(delete(hashTablePtr, toon) == 0)
+    {
+        printf("BAD\n");
+    }
+    printf("hashmap index: %s, value: %s\n", keyTest, search(hashTablePtr, keyTest));
     return 0;
 }
